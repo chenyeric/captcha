dd1ef781ad9e49023267b00479dc7d399c7374d0
Eric Chen <eric@ericchen.me>
2013-05-03 19:13:16 -0700

    final trial

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ include("ea_core.php");
 
 $ea = new EA_Core();
 $ea->clean();
-$ea->init(3,3,15,0.1,0.8,1, 9);
+$ea->init(5,2,15,0.1,0.8,1, 10);
 $ea->start();
 //	public function init($age_per_layer, $layer_num, $equil_size, $mut_rate, $cross_rate, $dying_rate, $max_age){
 
