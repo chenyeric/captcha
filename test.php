9bdb3ba25e7e77e58ced955efb9db713fe7d5c73
Eric Chen <eric@ericchen.me>
2013-05-05 12:04:27 -0700

    new run

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ include("ea_core.php");
 
 $ea = new EA_Core();
 $ea->clean();
-$ea->init(5,2,20,0.1,0.8,1, 10);
+$ea->init(10,3,20,0.1,0.8,1, 50);
 $ea->start();
 //	public function init($age_per_layer, $layer_num, $equil_size, $mut_rate, $cross_rate, $dying_rate, $max_age){
 
